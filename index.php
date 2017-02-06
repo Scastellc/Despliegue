386d3630ed6addd7b31dceedb67ba0d80d461324
Salva <scastellc@gmail.com>
2017-02-06 20:22:56 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba de despliegue desde GitHub</title>
+
+</head>
+<body>
+
+	<h1>Despliegue desde GitHub</h1>
+
+	<?php
+		phpinfo();
+
+	?>
+
+</body>
+</html>
